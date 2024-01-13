17f8421a8954731b2dd9145d30b3d5e014c0db74
marcsilva <marciliodevjava@gmail.com>
2024-01-12 23:16:04 -0300

    Script para gerar as tabelas no banco de dados

--- prepara_banco.py
+++ prepara_banco.py
@@ -6,8 +6,9 @@ print('Conectando...')
 try:
     conn = mysql.connector.connect(
         host='127.0.0.1',
+        port='3306',
         user='root',
-        password='1234657890'
+        password='1234567890'
     )
 
 except mysql.connector.Error as err:
