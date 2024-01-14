86f8dbd47ba7ec3822ce29aee92372847a2383f0
marcsilva <marciliodevjava@gmail.com>
2024-01-14 19:25:58 -0300

    Update views

--- resource/views.py
+++ resource/views.py
@@ -16,18 +16,41 @@ def novo():
         return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
+
 @app.route('/editar/<int:id>')
 def editar(id):
-
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
 
     jogo = Jogos.query.filter_by(id=id).first()
     return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
 
+
 @app.route('/atualizar', methods=['POST', ])
 def atualizar():
-    pass
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+
+    db.session.add(jogo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+
+    jogo = Jogos.query.filter_by(id=id).first()
+    db.session.delete(jogo)
+    db.session.commit()
+
+    flash('Jogo {} deletado com sucesso'.format(jogo.nome))
+    return redirect(url_for('index'))
+
 
 @app.route('/criar', methods=['POST', ])
 def criar():
@@ -38,7 +61,7 @@ def criar():
     jogo = Jogos.query.filter_by(nome=nome).first()
     if jogo:
         flash("Esse jogo já existe {}".format(nome))
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
 
     novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
     db.session.add(novo_jogo)
