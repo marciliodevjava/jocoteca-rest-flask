12127077510e0bd7c1b2688974efaea89d0b0de9
marcsilva <marciliodevjava@gmail.com>
2024-01-15 18:22:28 -0300

    Update helpers

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,19 @@
 import os
+
 from jogoteca import app
 
+
 def recupera_imagem(id):
-    nome = f'capa{id}.jpg'
+    nome = f'capa{id}'
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if nome_arquivo == nome:
+        if nome in nome_arquivo:
             return nome_arquivo
 
     return 'capa_padrao.jpg'
+
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != 'capa_padrao.jpg':
+        caminho_arquivo = os.path.join(app.config['UPLOAD_PATH'], arquivo)
+        if os.path.exists(caminho_arquivo):
+            os.remove(caminho_arquivo)
